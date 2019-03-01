97e40f0f0257d7965e57ecc2bd7a89912c268b9f
Pergamene <smartz373@gmail.com>
2019-03-01 11:37:03 -0700

    Finished turn-controller.  Needs more tests.

--- internal_comps/gp_game/src/services/interface/mock/controllers/turn-controller.js
+++ internal_comps/gp_game/src/services/interface/mock/controllers/turn-controller.js
@@ -1,4 +1,5 @@
 import { getReducedBoard } from '../../../../../../gp_board/src/services/interface/mock/controllers/board-controller.js';
+import { getCombinedBoard } from '../../../../../../gp_board/src/services/board-manipulator.js';
 import { GAME_KEYWORDS } from '../../../../entities/game-keywords.js';
 import { getValidMoves} from '../../../../../../gp_board/src/services/move-validator';
 
@@ -28,9 +29,13 @@ import { getValidMoves} from '../../../../../../gp_board/src/services/move-valid
  * 
  */
 export function getPlayersRandomTurn(playerCode, playerPieces) {
-  let board = getReducedBoard();
-  debugger;
-  let playerPiece = _getRandomPieceWithMove(playerPieces, board);
+  let board = getCombinedBoard(getReducedBoard(), playerPieces);
+  let pieceWithMoves = _getRandomPieceWithMoves(playerPieces, board);
+  let playerPiece = pieceWithMoves.piece;
+  if (playerPiece === null) {
+    return null;
+  }
+  let move = _getRandomMoveFromPiece(pieceWithMoves.moves);
   // The following return values are the ones that need to be altered:
   // turn[0].playerPiece.x, turn[0].playerPiece.y, turn[0].movePlayerPiece.x, turn[0].movePlayerPiece.x
   return { 
@@ -38,23 +43,49 @@ export function getPlayersRandomTurn(playerCode, playerPieces) {
     turn:[
       {
         type: 'movePlayerPiece',
-        playerPiece: {
-          x: playerPieces[GAME_KEYWORDS.PAWN][0].x,
-          y: playerPieces[GAME_KEYWORDS.PAWN][0].y
-        },
+        playerPiece,
         movePlayerPiece: {
-          x: Math.floor(Math.random() * 8),
-          y: Math.floor(Math.random() * 2) + 3
+          x: move.x,
+          y: move.y
         }
       }
     ]
   };
 }
 
-function _getRandomPieceWithMove(playerPieces, board) {
-  let rand = Math.floor(Math.random() * playerPieces.length);
-  for (let i = 0; i < playerPieces.length; i++) {
-    pieceType = playerPieces[(rand + i) % playerPieces.length];
-    
+function _getRandomPieceWithMoves(playerPieces, board) {
+  let seed = Math.floor(Math.random() * 5);
+  let pieceType, piece, moves;
+  for (let i = 0; i < 6; i++) {
+    pieceType = _getRandomPieceType((seed + i) % 6);
+    for (let j = 0; j < playerPieces[pieceType].length; j++) {
+      piece = playerPieces[pieceType][(j + seed) % playerPieces[pieceType].length];
+      moves = getValidMoves(piece.x, piece.y, board);
+      if (moves.length > 0) {
+        return { piece, moves };
+      }
+    }
+  }
+  return null;
+}
+
+function _getRandomMoveFromPiece(moves) {
+  return moves[Math.floor(Math.random() * moves.length)];
+}
+
+function _getRandomPieceType(seed) {
+  switch(seed) {
+    case 0:
+      return GAME_KEYWORDS.PAWN;
+    case 1:
+      return GAME_KEYWORDS.ROOK;
+    case 2:
+      return GAME_KEYWORDS.BISHOP;
+    case 3:
+      return GAME_KEYWORDS.KNIGHT;
+    case 4:
+      return GAME_KEYWORDS.QUEEN;
+    case 5:
+      return GAME_KEYWORDS.KING;
   }
 }
